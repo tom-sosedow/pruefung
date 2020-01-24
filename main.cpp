910aa573c8781c1b0db8d25da5125192ebd7fa84
Tom Sosedow <tom@unitatopotcorn.fritz.box>
2020-01-24 14:49:13 +0100

    Try

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 void warnmeldung();
 
 int main(){
-
+    std::cout << "Jemand war hier";
     int choice0; // Auswahlvariable, die vom Nutzer anhand der Menüoptionen gesetzt wird und mit switch statements überprüft wird
     int exit = 0; // Exitvariable um das Programm auf Wunsch des Nutzers zu terminieren. Das Programm läuft solange in einem Looop, bis der Nutezr das Programm beenden möchte, damit exit = 1 setzt und die Bedingung für die while Schleife ncith mehr erfüllt ist.
     float gewicht; // Gewicht des Nutzers, welches an die Funktionen zur Berechnung weietrer Werte übergeben wird
